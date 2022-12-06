74c8e40dbb7a39f860f3d8de7ec6cb62c062d8d5
Daniel Ebonine <ebonined@gmail.com>
2022-12-06 05:42:14 +0100

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,72 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
+/**
+ * reverse_listint - Reverses listint_t list.
+ * @head: A pointer to reverse.
+ *
+ * Return: A the reversed list.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if is a palindrome.
+ * @head: A pointer to the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp, *swi, *cen;
+	size_t size = 0, i;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	temp = *head;
+	while (temp)
+	{
+		size++;
+		temp = temp->next;
+	}
+
+	temp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		temp = temp->next;
+
+	if ((size % 2) == 0 && temp->n != temp->next->n)
+		return (0);
+
+	temp = temp->next->next;
+	swi = reverse_listint(&temp);
+	cen = swi;
+
+	temp = *head;
+	while (swi)
+	{
+		if (temp->n != swi->n)
+			return (0);
+		temp = temp->next;
+		swi = swi->next;
+	}
+	reverse_listint(&cen);
+
+	return (1);
+}
