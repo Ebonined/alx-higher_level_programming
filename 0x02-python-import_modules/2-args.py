a388b3268702a6ba1c10aaae8437d9d070754be2
Daniel Ebonine <ebonined@gmail.com>
2022-12-07 22:43:02 +0100

    task 2 corrected 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
-from sys import argv
+if __name__ == "__main__":
+    from sys import argv
 
-if len(argv) == 2:
-    print("{} argument:".format(len(argv) - 1))
-elif len(argv) > 2:
-    print("{} arguments:".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+    elif len(argv) > 2:
+        print("{} arguments:".format(len(argv) - 1))
 
-for i,arg in enumerate(argv):
-    
-    if i == 0:
-        continue
-    else:
-        print("{0} {1}".format(i, arg))
+    for i,arg in enumerate(argv):
+        
+        if i == 0:
+            continue
+        else:
+            print("{0} {1}".format(i, arg))
